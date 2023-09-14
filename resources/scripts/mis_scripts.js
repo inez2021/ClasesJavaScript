42bd73486015fb8037cdea11970dea9945ce7873
inez2021 <clara_ines@live.com>
2023-09-13 21:22:04 -0400

    Cambios 13Sept

--- resources/scripts/mis_scripts.js
+++ resources/scripts/mis_scripts.js
@@ -1,1 +1,28 @@
 alert('Hello world!')
+nombre = prompt('Ingrese su nombre')
+
+nom = 'Ana';
+
+// Tipos: numericos, texto, booleanos, arreglos
+// Numeros: naturales, enteros, reales, racionales, irracionales, complejos
+
+n = 500000;
+nom = 'Pepe';
+//String - Cadena
+r = "nom";
+
+nombre = 'Buenas noches ' + nombre;
+
+alert(nombre);
+
+n = parseInt( prompt('Ingrese un numero'));
+m = parseInt(prompt('Ingrese otro numero'));
+/*
+rescatar valor ingresado por usuario -> '5'
+convertir cadena a entero -> 5
+asignar valor a n
+*/
+resultado = n + m;
+alert('La suma es:' + resultado);
+
+//TODO: Estructuras de control (condicionales y bucles)
