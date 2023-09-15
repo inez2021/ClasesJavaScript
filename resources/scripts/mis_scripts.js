111195239af9c8f26c8270480c2ab02cf27ceeae
inez2021 <clara_ines@live.com>
2023-09-14 21:14:05 -0400

    Clase 14Sept

--- resources/scripts/mis_scripts.js
+++ resources/scripts/mis_scripts.js
@@ -1,5 +1,5 @@
-alert('Hello world!')
-nombre = prompt('Ingrese su nombre')
+//alert('Hello world!')
+//nombre = prompt('Ingrese su nombre')
 
 nom = 'Ana';
 
@@ -10,19 +10,97 @@ n = 500000;
 nom = 'Pepe';
 //String - Cadena
 r = "nom";
+nombre = '';
 
 nombre = 'Buenas noches ' + nombre;
 
-alert(nombre);
+//alert(nombre);
 
-n = parseInt( prompt('Ingrese un numero'));
-m = parseInt(prompt('Ingrese otro numero'));
+//n = parseInt( prompt('Ingrese un numero'));
+//m = parseInt(prompt('Ingrese otro numero'));
 /*
 rescatar valor ingresado por usuario -> '5'
 convertir cadena a entero -> 5
 asignar valor a n
 */
-resultado = n + m;
-alert('La suma es:' + resultado);
+//resultado = n + m;
+//alert('La suma es:' + resultado);
 
 //TODO: Estructuras de control (condicionales y bucles)
+
+//tipo de dato booleano -> bool
+abierto = true; // false
+
+alert(!abierto);
+//true -> 'true', false -> 'false'
+//45 -> '45', 0 -> '0'
+
+abierto = !abierto;
+
+x = 5;
+
+
+y = x + 1;
+
+alert(5 > 3);
+
+// afuera esta lloviendo o es de noche?
+// Si
+
+
+edad = parseInt(prompt('Cuantos anios tiene?'));
+
+/*
+if(edad < 18){
+    alert('lo sentimos');
+
+}else{
+    if(edad > 65){
+        alert('Se ha establecido');
+    }
+    alert('Bienvenido');
+}
+*/
+if(edad >=18){
+    if(edad > 65){
+        alert('Se ha establecido');
+    }
+    alert('Bienvenido');
+}else{
+    alert('lo sentimos');
+}
+
+
+
+alert('LISTO');
+
+
+
+
+d = parseInt(prompt('Ingrese un numero de dia de la semana?'));
+
+if(d == 1){
+    console.log('Lunes');
+}else{
+    if(d == 2){
+        console.log('Martes');
+    }else{
+        if(d == 3){
+            console.log('Miercoles');
+        }else{
+            if(d == 4){
+                console.log('Jueves');
+            }else{
+                if(d == 5){
+                    console.log('Viernes');
+                }else{
+                    if(d == 6){
+                        console.log('Sabado');
+                    }else{
+                        console.log('Domingo');                            console.log('Martes');   
+                    }     
+                }
+            }
+        }
+    }
+}
